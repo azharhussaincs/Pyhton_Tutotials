2cb60d4cbad1f5358217ec676a877970dbf02329
azhar <your.email@example.com>
2024-10-02 03:03:26 -0700

    Lecture 2 about Syntax of python

---
+++ Lect2.py
@@ -0,0 +1,8 @@
+"""In Python, syntax is the set of rules that tells you how to write code properly so the computer can understand it.
+   It includes things like using correct indentation (spaces), the right keywords, and symbols.
+   If you follow Python's syntax, your code will run without errors.
+"""
+print("Lecture2 About Syntax")
+#cout(this is lecture 2)
+var =10
+print(var)
