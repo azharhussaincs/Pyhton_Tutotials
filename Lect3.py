348d4b041a0f50e5b7157e9cde402c554d80be94
azhar <your.email@example.com>
2024-10-02 23:15:05 -0700

    variable In Pyhton

--- Lect3.py
+++ Lect3.py
@@ -1,3 +1,9 @@
 # What Is  comment
 name="Jamia Pakistan" #this is varable that store name 
 print(name) #this line used to print name or varibale
+"""
+Multiline Comments written like these 
+this is my lecture 4
+in this lecture i explain  comments in python
+
+"""
---
+++ Variable+_lec4.py
@@ -0,0 +1,6 @@
+Student_Name="Jamia Pakistan"
+Student_age=24
+Student_education=" BSCS"
+print(Student_age)
+print(Student_Name)
+print(Student_education)
