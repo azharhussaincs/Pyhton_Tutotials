7620734679267c16e93a7a81b99cebca145bf8c2
azhar <your.email@example.com>
2024-10-04 02:37:47 -0700

    Lect5

--- Variable+_lec4.py
+++ Variable+_lec4.py
@@ -1,6 +1,10 @@
-Student_Name="Jamia Pakistan"
-Student_age=24
-Student_education=" BSCS"
+Student_Name="Jamia Pakistan" #this string data type
+Student_age=24 #this is integer 
+Student_education=" BSCS" #this is also string
+student_marks=98.9 #this is flout datatypes
+print(student_marks)
 print(Student_age)
 print(Student_Name)
 print(Student_education)
+
+
